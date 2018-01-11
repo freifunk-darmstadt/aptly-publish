8f6e7bd1e7e7d150559d6cd9b46294b61a84e987
Martin Weinelt <hexa@darmstadt.ccc.de>
2018-01-11 01:33:18 +0100

    fix publish.sh

--- publish.sh
+++ publish.sh
@@ -8,10 +8,10 @@ for file in $ARTIFACT_DIR/*.deb; do
 done
 
 # add files to repository
-curl ${CURL_OPTS} -u$APTLY_USER:$APTLY_PASSWORD -X POST ${APTLY_API_BASE}/repos/${REPONAME}/file/${CI_COMMIT_SHA}
+curl ${CURL_OPTS} -u$APTLY_USER:$APTLY_PASSWORD -X POST ${APTLY_API_BASE}/repos/${PREFIX}/file/${CI_COMMIT_SHA}
 
 # create snapshot
-curl ${CURL_OPTS} -u$APTLY_USER:$APTLY_PASSWORD -X POST -H "Content-Type: application/json" --data "{\"Name\":\"${CI_COMMIT_SHA}\"}" ${APTLY_API_BASE}/repos/${REPONAME}/snapshots
+curl ${CURL_OPTS} -u$APTLY_USER:$APTLY_PASSWORD -X POST -H "Content-Type: application/json" --data "{\"Name\":\"${CI_COMMIT_SHA}\"}" ${APTLY_API_BASE}/repos/${PREFIX}/snapshots
 
 # update published snapshot
-curl ${CURL_OPTS} -u$APTLY_USER:$APTLY_PASSWORD -X PUT -H "Content-Type: application/json" --data "{\"Snapshots\": [{\"Component\":\"main\", \"Name\": \"$CI_COMMIT_SHA\"}]}" ${APTLY_API_BASE}/publish/${REPONAME}/${CODENAME}
+curl ${CURL_OPTS} -u$APTLY_USER:$APTLY_PASSWORD -X PUT -H "Content-Type: application/json" --data "{\"Snapshots\": [{\"Component\":\"main\", \"Name\": \"$CI_COMMIT_SHA\"}]}" ${APTLY_API_BASE}/publish/${PREFIX}/${DISTRIBUTION}
