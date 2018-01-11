3e29abb2f2618afbe224680290a4315284895ad1
Martin Weinelt <hexa@darmstadt.ccc.de>
2018-01-11 01:39:27 +0100

    move it into production

--- publish.sh
+++ publish.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-set -ex
+set -e
 
 # upload files
+echo "Uploading files"
 for file in $ARTIFACT_DIR/*.deb; do 
 	curl ${CURL_OPTS} -u$APTLY_USER:$APTLY_PASSWORD -X POST -F file=@${file} ${APTLY_API_BASE}/files/${CI_COMMIT_SHA}
 done
 
 # add files to repository
+echo "Adding files to repository"
 curl ${CURL_OPTS} -u$APTLY_USER:$APTLY_PASSWORD -X POST ${APTLY_API_BASE}/repos/${PREFIX}/file/${CI_COMMIT_SHA}
 
 # create snapshot
+echo "Creating snapshot"
 curl ${CURL_OPTS} -u$APTLY_USER:$APTLY_PASSWORD -X POST -H "Content-Type: application/json" --data "{\"Name\":\"${CI_COMMIT_SHA}\"}" ${APTLY_API_BASE}/repos/${PREFIX}/snapshots
 
 # update published snapshot
+echo "Updating published snapshot"
 curl ${CURL_OPTS} -u$APTLY_USER:$APTLY_PASSWORD -X PUT -H "Content-Type: application/json" --data "{\"Snapshots\": [{\"Component\":\"main\", \"Name\": \"$CI_COMMIT_SHA\"}]}" ${APTLY_API_BASE}/publish/${PREFIX}/${DISTRIBUTION}
