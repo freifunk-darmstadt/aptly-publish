fc482e819d5b2fd2dfad3455fda75e9fa70d3d6e
Martin Weinelt <hexa@darmstadt.ccc.de>
2018-01-14 04:26:18 +0100

    reference repository name in snapshot name

--- publish.sh
+++ publish.sh
@@ -14,8 +14,8 @@ curl ${CURL_OPTS} -u$APTLY_USER:$APTLY_PASSWORD -X POST ${APTLY_API_BASE}/repos/
 
 # create snapshot
 echo "Creating snapshot"
-curl ${CURL_OPTS} -u$APTLY_USER:$APTLY_PASSWORD -X POST -H "Content-Type: application/json" --data "{\"Name\":\"${CI_COMMIT_SHA}\"}" ${APTLY_API_BASE}/repos/${REPOSITORY}/snapshots
+curl ${CURL_OPTS} -u$APTLY_USER:$APTLY_PASSWORD -X POST -H "Content-Type: application/json" --data "{\"Name\":\"${REPOSITORY}/${CI_COMMIT_SHA}\"}" ${APTLY_API_BASE}/repos/${REPOSITORY}/snapshots
 
 # update published snapshot
 echo "Updating published snapshot"
-curl ${CURL_OPTS} -u$APTLY_USER:$APTLY_PASSWORD -X PUT -H "Content-Type: application/json" --data "{\"Snapshots\": [{\"Component\":\"main\", \"Name\": \"$CI_COMMIT_SHA\"}]}" ${APTLY_API_BASE}/publish/${REPOSITORY}/${DISTRIBUTION}
+curl ${CURL_OPTS} -u$APTLY_USER:$APTLY_PASSWORD -X PUT -H "Content-Type: application/json" --data "{\"Snapshots\": [{\"Component\":\"main\", \"Name\": \"${REPOSITORY}/${CI_COMMIT_SHA}\"}]}" ${APTLY_API_BASE}/publish/${REPOSITORY}/${DISTRIBUTION}
